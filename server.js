d7db92963850a9d0b3fc90b58c98c28c1a146849
Wuild <wuildz@gmail.com>
2024-09-27 23:56:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/*', async (req, res) => {
         await browser.close();
 
         // Cache the rendered content
-        setCachedContent(cacheKey, content);
+        // setCachedContent(cacheKey, content);
 
         logger.info(`Successfully prerendered URL: ${targetUrl}`);
         res.send(content);
